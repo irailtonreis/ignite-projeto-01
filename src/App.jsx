a47082d2eb0d4f4e680f73bcb000b685dffe74aa
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-14 17:42:53 -0300

    feat: Estrutura da aplicação - sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react'
 import { Post } from './Post'
 import { Header } from './components/Header'
+import { Sidebar } from './Sidebar'
+
+
 import './global.css'
+import styles from './App.module.css'
 
 function App() {
   return (
     <>
-      <Header />
-      <Post
-        author="Deigo"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolores quidem consequuntur odit cumque. Velit, beatae modi itaque repellendus deleniti reprehenderit maxime dolores quo expedita id impedit aperiam voluptas architecto."
-      />
-      <Post
-        author="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolores quidem consequuntur odit cumque. Velit, beatae modi itaque repellendus deleniti reprehenderit maxime dolores quo expedita id impedit aperiam voluptas architecto."
-      />
+       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+          author="Deigo"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolores quidem consequuntur odit cumque. Velit, beatae modi itaque repellendus deleniti reprehenderit maxime dolores quo expedita id impedit aperiam voluptas architecto."
+        />
+        <Post
+          author="Gabriel"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolores quidem consequuntur odit cumque. Velit, beatae modi itaque repellendus deleniti reprehenderit maxime dolores quo expedita id impedit aperiam voluptas architecto."
+        />
+        </main>
+      </div>  
     </>
   )
 }
