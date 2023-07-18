bdf84ffcaa6236c2b504d82819479b47c9162729
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-18 16:15:56 -0300

    feat: Propriedades do post

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,34 @@ import { Sidebar } from './Sidebar'
 import './global.css'
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Dragarnath.png',
+      name: 'Josue Pereira',
+      role: 'Devops'
+    },
+    publishedAt: new Date ('2023-07-16 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Felipe Santos',
+      role: 'Financeiro',
+    },
+    
+    content: [
+     { type: 'paragraph', content: ' Fala galeraa 👋' },
+     { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+     { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date ('2023-07-17 20:00:00'),
+  }
+  
+]
+
 function App() {
   return (
     <>
@@ -14,14 +42,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post
-          author="Deigo"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolores quidem consequuntur odit cumque. Velit, beatae modi itaque repellendus deleniti reprehenderit maxime dolores quo expedita id impedit aperiam voluptas architecto."
-        />
-        <Post
-          author="Gabriel"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolores quidem consequuntur odit cumque. Velit, beatae modi itaque repellendus deleniti reprehenderit maxime dolores quo expedita id impedit aperiam voluptas architecto."
-        />
+        {posts.map (post => {
+        return (
+           <Post
+           author={post.author}
+           content={post.content}
+           publishedAt={post.publishedAt}
+           />
+        )
+      })}
         </main>
       </div>  
     </>
